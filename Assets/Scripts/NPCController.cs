0a61c55c8bd48fae9656904c2a9f2dd0eae06c44
Jonathan Müller <JoyeMueller@aol.com>
2020-03-16 17:52:56 +0100

    Added Point Click Controller. Added Nav Mesh Component. Added NPC Mover and Controller

---
+++ Assets/Scripts/NPCController.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NPCController : MonoBehaviour
+{
+    public LayerMask m_TargetLayer;
+
+    private List<NPCMove> m_Npcs = new List<NPCMove>(); 
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetMouseButtonDown(1))
+        {
+            Ray ray = GetComponent<Camera>().ScreenPointToRay(Input.mousePosition);
+            RaycastHit rayHit;
+            if (Physics.Raycast(ray, out rayHit, m_TargetLayer)) 
+            {
+                //Debug.Log("Commanding units to: " + rayHit.point);
+                foreach (NPCMove npc in m_Npcs)
+                {
+                    npc.SetDestination(rayHit.point);
+                }
+            }
+        }
+    }
+
+    public void RegisterNPC(NPCMove _Target)
+    {
+        if(!CheckNpcsForElement(_Target))
+        {
+            m_Npcs.Add(_Target);
+        }
+    }
+
+    private bool CheckNpcsForElement(NPCMove _Target)
+    {
+        foreach (NPCMove npc in m_Npcs)
+        {
+            if(npc.gameObject == _Target.gameObject)
+            {
+                return true;
+            }
+        }
+
+        return false;
+    }
+}
---
+++ Assets/Scripts/NPCMove.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class NPCMove : MonoBehaviour
+{
+    NavMeshAgent _navMeshAgent; 
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        if(GetComponent<NavMeshAgent>() != null)
+        {
+            _navMeshAgent = GetComponent<NavMeshAgent>();
+        }else {
+            Debug.LogError("There is no agent on this npc " + gameObject.name);
+            this.enabled = false;
+        }
+
+        try 
+        {
+            GameObject.FindGameObjectWithTag("MainCamera").GetComponent<NPCController>().RegisterNPC(this);
+        }catch (Exception ex)
+        {
+            Debug.LogError("Could not register " + gameObject.name + " as an npc. " + ex.Message);
+        }
+    }
+
+    public void SetDestination(Vector3 target)
+    {
+        //Debug.Log("Hey, im moving to: " + target);
+        _navMeshAgent.SetDestination(target);
+    }
+}
